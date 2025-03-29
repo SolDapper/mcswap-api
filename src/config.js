92c4a754f9d93d207c4cfa54e6356e535146466c
SolDapper <soldapper.com@gmail.com>
2025-03-29 10:08:49 -0400

    modified:   src/config.js

--- src/config.js
+++ src/config.js
@@ -3,9 +3,8 @@ import 'dotenv/config';
 import Express from 'express';
 // *********************************************************************************
 
-var whitelist  = ['http://www.mcswap.xyz', 'http://localhost:3300'];
 var host = "http://api.mcswap.xyz"; // your live domain
-host = "http://localhost"; // comment out for prod
+// host = "http://localhost"; // comment out for prod
 var treasury = "ACgZcmgmAnMDxXxZUo9Zwg2PS6WQLXy63JnnLmJFYxZZ"; // optional affiliate treasury
 var fee = "0"; // optional affiliate fee
 var cleanup = 60000; // cleanup every n miliseconds
