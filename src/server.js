3d2d31f8879b25edf031dc762672737fb3ee5207
SolDapper <soldapper.com@gmail.com>
2025-03-29 07:52:14 -0400

    modified:   src/server.js

--- src/server.js
+++ src/server.js
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
 // *********************************************************************************
 // include actions
 import { payment } from './actions/payment.js';
-app.use("/payment", payment);
+app.use("/payment/*/*/*", payment);
 // import { queue } from './actions/queue.js';
 // app.use("/queue/*", queue);
 // *********************************************************************************
