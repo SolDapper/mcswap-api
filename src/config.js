169bc1f6f33ad700eaede37f2bc4d17e6a657e98
SolDapper <soldapper.com@gmail.com>
2025-03-29 08:54:22 -0400

    modified:   src/config.js

--- src/config.js
+++ src/config.js
@@ -11,17 +11,6 @@ var whitelist  = ['http://www.mcswap.xyz', 'http://localhost:3300'];
 var fee = "0";
 
 // *********************************************************************************
-const referrerCheck = (req, res, next) => {
-    const referrer = req.get('referer');
-    if (referrer && whitelist.includes(new URL(referrer).origin)) {
-      next();
-    } else {
-      res.status(403).send('Access Denied');
-    }
-};
-const app  = Express();
-app.use(referrerCheck);
-// *********************************************************************************
 if(host.includes("localhost")){host=host+":3300";}
 var rpc = process.env.RPC;
 export var host, auto, rpc, treasury, fee;
