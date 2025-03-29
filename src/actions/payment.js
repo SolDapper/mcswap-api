5862f9650b4b4b11d358500cbcf07632929e5a74
SolDapper <soldapper.com@gmail.com>
2025-03-29 08:49:01 -0400

    modified:   src/actions/payment.js 	new file:   src/queue/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.txt

--- src/actions/payment.js
+++ src/actions/payment.js
@@ -11,6 +11,7 @@ payment.get('/payment/:format/:escrow/:reference',async(req,res)=>{
     const _format_ = req.params.format;
     const format = _format_.toLowerCase();
     const _escrow_ = req.params.escrow;
+    const reference = req.params.reference;
     const request = {
         "rpc":rpc,
         "display":true,
@@ -32,8 +33,15 @@ payment.get('/payment/:format/:escrow/:reference',async(req,res)=>{
             const obj = {}
             obj.label = "Purchase "+_format_+" "+name;
             obj.icon = image;
-            fs.writeFile('./src/queue/'+reference+'.txt',Date.now(),(err)=>{if(err){res.status(400).json(err);}});
-            res.status(200).json(obj);
+            const time = Date.now();
+            try{
+                const path = './src/queue/'+reference+'.txt';
+                fs.writeFileSync(path,time.toString());
+                res.status(200).json(obj);
+            }
+            catch(err){
+                res.status(400).json(err);
+            }
         }
         catch(err){
             res.status(400).json(err);
