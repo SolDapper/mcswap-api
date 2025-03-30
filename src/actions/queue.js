a611a5cb25c5262d2c57f4db8905afcc2bff6276
SolDapper <soldapper.com@gmail.com>
2025-03-29 23:25:40 -0400

    modified:   src/actions/queue.js

--- src/actions/queue.js
+++ src/actions/queue.js
@@ -2,9 +2,9 @@
 import Express from 'express';
 import fs from 'fs';
 const queue = Express.Router();
-queue.get('/queue/:reference',async(req,res)=>{
+queue.get('/scanner/:reference',async(req,res)=>{
     const reference = req.params.reference;
-    if(fs.existsSync("./src/queue/"+reference+".txt")){
+    if(fs.existsSync("/tmp/"+reference+".txt")){
         res.status(200).json("scanned");
     }
     else{
