362411c308a3eef83a7026066048d52e9173b6c0
SolDapper <soldapper.com@gmail.com>
2025-03-29 23:01:36 -0400

    modified:   src/actions/payment.js

--- src/actions/payment.js
+++ src/actions/payment.js
@@ -35,15 +35,10 @@ payment.get('/payment/:format/:escrow',async(req,res)=>{
             obj.icon = image;
             const time = Date.now();
             try{
-
                 console.log(import.meta.dirname);
-                // if (!fs.existsSync("../queue")) {
-                //     console.log("queue directory not found");
-                // }
                 const temp_file = '../queue/'+reference+'.txt';
                 console.log(temp_file);
-                
-                // fs.writeFileSync(temp_file,time.toString());
+                fs.writeFileSync(temp_file,time.toString());
                 res.status(200).json(obj);
             }
             catch(err){
