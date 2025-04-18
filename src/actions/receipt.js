6edba239ac6cbb42a401fc4fdab4a34301ded4f2
SolDapper <soldapper.com@gmail.com>
2025-04-18 15:49:20 -0400

    modified:   src/actions/receipt.js

--- src/actions/receipt.js
+++ src/actions/receipt.js
@@ -9,8 +9,8 @@ receipt.post('/receipt', cors(filter), async(err,req,res,next)=>{
     }
     else{
 
-        // const body  = req.body;
-        res.status(200).json("debug");
+        const body  = req.body;
+        res.status(200).json(body);
         
         // const reference = req.params.reference;
         // if(fs.existsSync("/tmp/"+reference+".txt")){
