2676c6b1ab72525c88847c0b7e8c1e832b505dd0
SolDapper <soldapper.com@gmail.com>
2025-04-18 14:34:47 -0400

    modified:   src/actions/ping.js

--- src/actions/ping.js
+++ src/actions/ping.js
@@ -2,18 +2,17 @@
 import {whitelist} from '../config.js';
 import Express from 'express';
 const ping = Express.Router();
-const options = {
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    }
-};
+// const options = {
+//     origin: function (origin, callback) {
+//       if (whitelist.indexOf(origin) !== -1 || !origin) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error('Not allowed by CORS'));
+//       }
+//     }
+// };
 // ping.get('/ping', cors(options), async(req,res)=>{
 ping.get('/ping', async(req,res)=>{
-  res.status(200).json(res);
-  // res.status(200).json("ok");
+  res.status(200).json("ok");
 });
 export {ping};
