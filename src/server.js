eb8dddba87445c01975b6f92f2c5097eedc0bdab
SolDapper <soldapper.com@gmail.com>
2025-04-18 15:13:22 -0400

    modified:   src/server.js

--- src/server.js
+++ src/server.js
@@ -59,7 +59,7 @@ const whitelistFilter = {
 };
 app.get('/ping', cors(whitelistFilter), async(err,req,res,next)=>{
   if(err.message){
-    res.status(200).json(err);
+    res.status(200).json(err.message);
   }
   else{
     res.status(200).json("ok");
