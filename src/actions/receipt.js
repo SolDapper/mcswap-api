b55fa5c99dc1ff8567d938a4e18b620702ad6e6b
SolDapper <soldapper.com@gmail.com>
2025-04-18 16:29:51 -0400

    modified:   src/actions/receipt.js

--- src/actions/receipt.js
+++ src/actions/receipt.js
@@ -9,39 +9,30 @@ receipt.post('/receipt', cors(filter), async(err,req,res,next)=>{
     //     res.status(200).json("Access Denied!");
     // }
     // else{
-
         const body  = req.body;
-        // res.status(200).json(body);
-
-        console.log(process.env.MAIL_EMAIL);
-
-        // let transporter = nodemailer.createTransport({
-        //     service: 'gmail', 
-        //     auth: {
-        //         user: process.env.MAIL_EMAIL,
-        //         pass: process.env.MAIL_PASS
-        //     }
-        // });
-        // console.log(transporter);
-
-        // let mailOptions = {
-        //     from: body.Seller_Email, // Sender address
-        //     to: body.Buyer_Email, // List of recipients
-        //     subject: 'Asset Purchase', // Subject line
-        //     text: 'Hello from Node.js!', // Plain text body
-        //     html: '<b>Hello from Node.js!</b>' // HTML body
-        // };
-
-        // transporter.sendMail(mailOptions, (error, info) => {
-        //     if (error) {
-        //         res.status(200).json("error");
-        //     }
-        //     else{
-        //         res.status(200).json("ok");
-        //     }
-        // });
-
+        let transporter = nodemailer.createTransport({
+            service: 'gmail', 
+            auth: {
+                user: process.env.MAIL_EMAIL,
+                pass: process.env.MAIL_PASS
+            }
+        });
+        // to: body.Buyer_Email+","+body.Seller_Email,
+        let mailOptions = {
+            from: body.Seller_Email,
+            to: body.Buyer_Email,
+            subject: 'Asset Purchase Receipt',
+            text: 'Hello from Node.js!',
+            html: '<b>Hello from Node.js!</b>'
+        };
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                res.status(200).json("error");
+            }
+            else{
+                res.status(200).json("ok");
+            }
+        });
     // }
-
 });
 export {receipt};
