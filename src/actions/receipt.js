47536b76817fba284938d2082ebd397576414563
SolDapper <soldapper.com@gmail.com>
2025-04-18 22:27:10 -0400

    modified:   src/actions/receipt.js

--- src/actions/receipt.js
+++ src/actions/receipt.js
@@ -11,23 +11,25 @@ receipt.post('/receipt', cors(filter), async(err,req,res,next)=>{
     // else{
         const body  = req.body;
         let msg = '';
-
         msg += '<table cellpadding="0" cellspacing="0" width="100%">';
-        msg += '<tr>';
-        msg += '<td>&nbsp;a</td>';
-        msg += '<td width="600">b</td>';
-        msg += '<td>&nbsp;c</td>';
-        msg += '</tr>';
+        msg += '<tr><td>Asset Name: </td><td width="600">'+body.Asset_Name+'</td></tr>';
+        msg += '<tr><td>Description: </td><td width="600">'+body.Asset_Name+'</td></tr>';
+        msg += '<tr><td>Seller Email: </td><td width="600">'+body.Seller_Email+'</td></tr>';
+        msg += '<tr><td>Buyer Email: </td><td width="600">'+body.Buyer_Email+'</td></tr>';
+        msg += '<tr><td>Buyer Name: </td><td width="600">'+body.Buyer_Name+'</td></tr>';
+        msg += '<tr><td>Buyer Address: </td><td width="600">'+body.Buyer_Address+'</td></tr>';
+        msg += '<tr><td>Image: </td><td width="600">'+body.Image+'</td></tr>';
+        msg += '<tr><td>Format: </td><td width="600">'+body.Format+'</td></tr>';
+        msg += '<tr><td>From: </td><td width="600">'+body.From+'</td></tr>';
+        msg += '<tr><td>To: </td><td width="600">'+body.To+'</td></tr>';
+        msg += '<tr><td>Token: </td><td width="600">'+body.Token+'</td></tr>';
+        msg += '<tr><td>Mint: </td><td width="600">'+body.Mint+'</td></tr>';
+        msg += '<tr><td>Tokens Paid: </td><td width="600">'+body.Tokens_Paid+'</td></tr>';
+        msg += '<tr><td>SOL Paid: </td><td width="600">'+body.SOL_Paid+'</td></tr>';
+        msg += '<tr><td>SOL Paid: </td><td width="600">'+body.SOL_Paid+'</td></tr>';
+        msg += '<tr><td>Listing: </td><td width="600">'+body.Listing+'</td></tr>';
+        msg += '<tr><td>Signature: </td><td width="600"><a target="_blank" href="https://solana.fm/tx/'+body.Signature+'">View Transaction</a></td></tr>';
         msg += '</table>';
-
-
-
-
-
-
-
-
-
         let transporter = nodemailer.createTransport({
             service: 'gmail', 
             auth: {
