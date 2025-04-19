39df3c8508ca9d54b38a5eb552284e4f0d278410
SolDapper <soldapper.com@gmail.com>
2025-04-18 22:37:55 -0400

    modified:   src/actions/receipt.js

--- src/actions/receipt.js
+++ src/actions/receipt.js
@@ -12,22 +12,22 @@ receipt.post('/receipt', cors(filter), async(err,req,res,next)=>{
         const body  = req.body;
         let msg = '';
         msg += '<table cellpadding="0" cellspacing="0" width="100%">';
-        msg += '<tr><td><img width="300" src="'+body.Image+'" /></td></tr>';
-        msg += '<tr><td width="100">Asset Name: </td><td width="600">'+body.Asset_Name+'</td></tr>';
-        msg += '<tr><td width="100">Description: </td><td width="600">'+body.Asset_Name+'</td></tr>';
-        msg += '<tr><td width="100">Seller Email: </td><td width="600">'+body.Seller_Email+'</td></tr>';
-        msg += '<tr><td width="100">Buyer Email: </td><td width="600">'+body.Buyer_Email+'</td></tr>';
-        msg += '<tr><td width="100">Buyer Name: </td><td width="600">'+body.Buyer_Name+'</td></tr>';
-        msg += '<tr><td width="100">Buyer Address: </td><td width="600">'+body.Buyer_Address+'</td></tr>';
-        msg += '<tr><td width="100">Format: </td><td width="600">'+body.Format+'</td></tr>';
-        msg += '<tr><td width="100">Listing: </td><td width="600">'+body.Listing+'</td></tr>';
-        msg += '<tr><td width="100">Seller: </td><td width="600">'+body.Seller+'</td></tr>';
-        msg += '<tr><td width="100">Buyer: </td><td width="600">'+body.Buyer+'</td></tr>';
-        msg += '<tr><td width="100">Token Symbol: </td><td width="600">'+body.Token+'</td></tr>';
-        msg += '<tr><td width="100">Token Mint: </td><td width="600">'+body.Mint+'</td></tr>';
-        msg += '<tr><td width="100">Tokens Paid: </td><td width="600">'+body.Tokens_Paid+'</td></tr>';
-        msg += '<tr><td width="100">SOL Paid: </td><td width="600">'+body.SOL_Paid+'</td></tr>';
-        msg += '<tr><td width="100">Signature: </td><td width="600"><a target="_blank" href="https://solana.fm/tx/'+body.Signature+'">View Transaction</a></td></tr>';
+        msg += '<tr><td><img width="400" src="'+body.Image+'" /></td></tr>';
+        msg += '<tr><td style="width:100px">Asset Name: </td><td width="600">'+body.Asset_Name+'</td></tr>';
+        msg += '<tr><td style="width:100px">Description: </td><td width="600">'+body.Asset_Name+'</td></tr>';
+        msg += '<tr><td style="width:100px">Seller Email: </td><td width="600">'+body.Seller_Email+'</td></tr>';
+        msg += '<tr><td style="width:100px">Buyer Email: </td><td width="600">'+body.Buyer_Email+'</td></tr>';
+        msg += '<tr><td style="width:100px">Buyer Name: </td><td width="600">'+body.Buyer_Name+'</td></tr>';
+        msg += '<tr><td style="width:100px">Buyer Address: </td><td width="600">'+body.Buyer_Address+'</td></tr>';
+        msg += '<tr><td style="width:100px">Format: </td><td width="600">'+body.Format+'</td></tr>';
+        msg += '<tr><td style="width:100px">Listing: </td><td width="600">'+body.Listing+'</td></tr>';
+        msg += '<tr><td style="width:100px">Seller: </td><td width="600">'+body.Seller+'</td></tr>';
+        msg += '<tr><td style="width:100px">Buyer: </td><td width="600">'+body.Buyer+'</td></tr>';
+        msg += '<tr><td style="width:100px">Token Symbol: </td><td width="600">'+body.Token+'</td></tr>';
+        msg += '<tr><td style="width:100px">Token Mint: </td><td width="600">'+body.Mint+'</td></tr>';
+        msg += '<tr><td style="width:100px">Tokens Paid: </td><td width="600">'+body.Tokens_Paid+'</td></tr>';
+        msg += '<tr><td style="width:100px">SOL Paid: </td><td width="600">'+body.SOL_Paid+'</td></tr>';
+        msg += '<tr><td style="width:100px">Signature: </td><td width="600"><a target="_blank" href="https://solana.fm/tx/'+body.Signature+'">View Transaction</a></td></tr>';
         msg += '</table>';
         let transporter = nodemailer.createTransport({
             service: 'gmail', 
